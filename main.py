9dd18b97184eae3a7b4dbf655f562dc1ab6c54fc
kuranyimg <kuranyimg@gmail.com>
2024-10-16 18:24:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,15 +46,15 @@ class Bot(BaseBot):
         await self.highrise.react("heart", user.id)
 
     async def on_chat(self, user: User, message: str) -> None:
-       print(f"{user.username}: {message}")
+        print(f"{user.username}: {message}")
 
-       await handle_tip_command(self, message, user)  # Handle tipping commands
+        await handle_tip_command(self, message, user)  # Handle tipping commands
 
-       if message.startswith("Carteira") or  message.startswith("Wallet") or    message.startswith("wallet") or       message.startswith("carteira"):
-           if user.username in ["FallonXOXO", "sh1n1gam1699", "RayBM"]:  # Check if the user is allowed
-               wallet = (await self.highrise.get_wallet()).content
-               await self.highrise.send_whisper(user.id, f"AMOUNT : {wallet[0].amount} {wallet[0].type}")
-               await self.highrise.send_emote("emote-blowkisses")
+        if message.startswith("Carteira") or  message.startswith("Wallet") or    message.startswith("wallet") or       message.startswith("carteira"):
+            if user.username in ["FallonXOXO", "sh1n1gam1699", "RayBM"]:  # Check if the user is allowed
+                wallet = (await self.highrise.get_wallet()).content
+                await self.highrise.send_whisper(user.id, f"AMOUNT : {wallet[0].amount} {wallet[0].type}")
+                await self.highrise.send_emote("emote-blowkisses")
 
 
         if message.lower() == "/fish":
