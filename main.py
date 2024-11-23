ca01719578d934a3a75980ce8f3e09fe5dd3e5c0
kuranyimg <kuranyimg@gmail.com>
2024-11-23 04:39:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ pescar = ["🥈YOU WON THE MEDAL: SILVER FISHERMAN🥈","🥉YOU WON THE MEDAL: 
 class Bot(BaseBot):
     async def on_start(self, session_metadata: SessionMetadata) -> None:
         print("working")
-        await self.highrise.walk_to(Position(17.0 , 0.0 , 18.5, "FrontLeft"))
+        await self.highrise.walk_to(Position(17.0 , 0.0 , 20.5, "FrontLeft"))
       
     async def on_user_join(self, user: User, position: Position | AnchorPosition) -> None:
         # Only the bot prints the message in the console
