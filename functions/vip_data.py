cc6937df3f2bd4e3a2be75182044f86facce8a3d
kuranyimg <kuranyimg@gmail.com>
2025-05-01 19:53:48 +0300

    Create vip_data.py

---
+++ functions/vip_data.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+VIP_FILE = "vip_list.json"
+
+def load_vip_list():
+    if not os.path.exists(VIP_FILE):
+        return set()
+    with open(VIP_FILE, "r") as f:
+        return set(json.load(f))
+
+def save_vip_list(vip_list):
+    with open(VIP_FILE, "w") as f:
+        json.dump(sorted(list(vip_list)), f)
