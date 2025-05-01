e31b04151621bc34df3b4ec2b9f0b53ad232a80c
kuranyimg <kuranyimg@gmail.com>
2025-05-01 20:17:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,17 @@ class Bot(BaseBot):
             vip_message = get_vip_list(vip_list)
             await self.highrise.send_whisper(user.id, vip_message)
             return
+            
+        if message.lower().startswith("loop "):
+            await loop(self, user, message)
+            return
+
+        if message.lower().startswith("stop"):
+            await stop_loop(self, user, message)
+            return
+
+        # Check for direct emote names
+        await check_and_start_emote_loop(self, user, message)
 
         if message.startswith("/carp"):
            await self.highrise.react("clap",user.id)
