b93add04b1955921cb8dd4878f408dc8aa6f945b
kuranyimg <kuranyimg@gmail.com>
2025-05-04 11:00:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ class Bot(BaseBot):
             "delay": 12.5
         },
 
-        "Relaxed":[
-      "sit-relaxed",
-      29.889858,
-      false
-   ],
-   "Attentive":[
-      "idle_layingdown",
-      24.585168,
-      false
-   }
+        "relaxed": {
+            "emote": "sit-relaxed",
+            "delay": 29.889858,
+        },
+
+        "att": {
+            "emote": "idle_layingdown",
+            "delay": 24.585168,
+        }
+    }
 
     user_loops = {}
 
@@ -36,6 +36,7 @@ class Bot(BaseBot):
     async def on_whisper(self, user: User, message: str) -> None:
         print(f"[WHISPER] {user.username}: {message}")
         message = message.strip().lower()
+        
         if message == "stop":
             # Stop the loop
             loop_data = self.user_loops.get(user.user_id)
@@ -43,6 +44,7 @@ class Bot(BaseBot):
                 loop_data['loop'].cancel()
                 self.user_loops.pop(user.user_id, None)
             await self.highrise.chat(f"Stopped emote loop for {user.username}.")
+        
         elif message in self.emote_dict:
             command = self.emote_dict[message]
             # Start the loop for the user
@@ -50,7 +52,9 @@ class Bot(BaseBot):
                 loop_task = asyncio.create_task(self.send_emote_continuously(command, user.user_id))
                 self.user_loops[user.user_id] = {'command': command, 'loop': loop_task}
                 await self.highrise.chat(f"Started emote loop for {user.username}: {message}")
-        elif message == "loop" or message == "Loop" or message == "!loop" or message == "-loop":
+
+        # Handle the 'loop' command variants in whisper as well
+        elif message in ["loop", "Loop", "!loop", "-loop"]:
             # If the loop command is received, start the loop for the user
             if user.user_id not in self.user_loops:
                 loop_task = asyncio.create_task(self.send_emote_continuously(self.emote_dict["dance"], user.user_id))
