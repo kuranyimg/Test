f35dd12a5aba98e218e8c697d2d67d508482ae26
kuranyimg <kuranyimg@gmail.com>
2025-05-08 02:44:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import asyncio
-from highrise import BaseBot, Position
-from highrise.models import SessionMetadata, User, AnchorPosition
+from highrise import Highrise, BaseBot
+from highrise.models import User, Position
 from functions.loop_emote import check_and_start_emote_loop, handle_user_movement
 
-class Bot(BaseBot):
-    async def on_start(self, session_metadata: SessionMetadata) -> None:
-        self.user_loops = {}
-        await self.highrise.walk_to(Position(17.5, 0.0, 12.5, "FrontRight"))
+class MyBot(BaseBot):
+    def __init__(self):
+        super().__init__()
+        self.user_loops = {}  # key: user.id, value: { stop_event, pause_event, task }
+
+    async def on_start(self, highrise: Highrise):
+        self.highrise = highrise
         print("Bot is ready.")
 
-    async def on_chat(self, user: User, message: str) -> None:
+    async def on_chat(self, user: User, message: str):
         await check_and_start_emote_loop(self, user, message)
 
-    async def on_whisper(self, user: User, message: str) -> None:
+    async def on_whisper(self, user: User, message: str):
         await check_and_start_emote_loop(self, user, message)
 
-    async def on_user_move(self, user: User, pos: Position | AnchorPosition) -> None:
+    async def on_user_move(self, user: User, pos: Position):
         await handle_user_movement(self, user)
+
+if __name__ == "__main__":
+    MyBot().run()
