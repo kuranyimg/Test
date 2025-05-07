26311dc4a743fc88f0c12462be17a7f0ef83e508
kuranyimg <kuranyimg@gmail.com>
2025-05-08 02:35:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from functions.loop_emote import check_and_start_emote_loop, handle_user_movemen
 
 class Bot(BaseBot):
     async def on_start(self, session_metadata: SessionMetadata) -> None:
-        self.user_loops = {}  # لتخزين حالة التكرار لكل مستخدم
+        self.user_loops = {}
         await self.highrise.walk_to(Position(17.5, 0.0, 12.5, "FrontRight"))
         print("Bot is ready.")
 
@@ -14,6 +14,6 @@ class Bot(BaseBot):
 
     async def on_whisper(self, user: User, message: str) -> None:
         await check_and_start_emote_loop(self, user, message)
-        
+
     async def on_user_move(self, user: User, pos: Position | AnchorPosition) -> None:
-        await handle_user_movement(self, user, pos)
+        await handle_user_movement(self, user)
