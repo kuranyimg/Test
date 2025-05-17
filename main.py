7d55d75a2a6bbae87eba34c7562eb2258dbd5b80
kuranyimg <kuranyimg@gmail.com>
2025-05-17 23:54:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import asyncio
 from highrise import BaseBot, Position
 from highrise.models import SessionMetadata, User, AnchorPosition
 from functions.loop_emote import (
-    start_loop_for_user,
-    handle_user_movement,
     check_and_start_emote_loop,
+    handle_user_movement,
     emote_list
 )
 from functions.json import bot_location
