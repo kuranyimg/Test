fa20f325460e809b219d294bb0225cb3f5581ccc
kuranyimg <kuranyimg@gmail.com>
2025-06-26 05:54:46 +0300

    Create Free.py

---
+++ functions/Free.py
@@ -0,0 +1,24 @@
+{
+  "face_hair": {
+    "face_hair-n_newbasicfacehairupper17": "Chevron Mustache",
+    "face_hair-n_newbasicfacehairupper16": "Cantinflas",
+    "face_hair-n_newbasicfacehairupper15": "Walrus Mustache",
+    ...
+  },
+  "hair_front": {
+    "hair_front-n_malenew33": "Short Short Fro",
+    "hair_front-n_malenew32": "Box Braids",
+    ...
+  },
+  "hair_back": {
+    "hair_back-n_malenew33": "Short Short Fro",
+    "hair_back-n_malenew32": "Box Braids",
+    ...
+  },
+  "top": {
+    ...
+  },
+  "bottom": {
+    ...
+  }
+}
